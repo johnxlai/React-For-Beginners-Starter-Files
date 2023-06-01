76e723f992dfc4bf7d5b51c7e8eff0c3aaa7bb09
John Lai <lai.john.py@gmail.com>
2023-05-31 21:51:51 -0400

    Add Form store selector component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <form className="store-selector">
+        <h2>Please Enter a Store</h2>
+        <input type="text" name="" id="" placeholder="Store Name" />
+        <button type="submit">Visit store</button>
+      </form>
+    );
+  }
+}
+
+export default StorePicker;
