269dbac74c4203c45b2c6f5a904eaae7310f8a6b
John Lai <lai.john.py@gmail.com>
2023-06-05 15:49:35 -0400

    Add Add fish form

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
   render() {
-    return <h1>test</h1>;
+    return (
+      <div className="inventory">
+        <h2>Inventory</h2>
+        <AddFishForm />
+      </div>
+    );
   }
 }
 
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,14 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    // 1.Stop the form
+    e.preventDefault();
+
+    //2. get the txt from that input
+    const storeName = this.myInput.current.value;
+    //3. Change trhe page to /store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+  };
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
         <input
           type="text"
           name=""
           id=""
+          ref={this.myInput}
           placeholder="Store Name"
           defaultValue={getFunName()}
         />
