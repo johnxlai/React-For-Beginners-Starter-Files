88a3e6ed2f58d405341491cef87ffdcba171a6d9
John Lai <lai.john.py@gmail.com>
2023-06-05 13:24:55 -0400

    Use helper function

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
-        <input type="text" name="" id="" placeholder="Store Name" />
+        <input
+          type="text"
+          name=""
+          id=""
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
         <button type="submit">Visit store</button>
       </form>
     );
