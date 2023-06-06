c2c69380e970047553c44d7dc6fb79d726446e99
John Lai <lai.john.py@gmail.com>
2023-06-06 14:40:04 -0400

    Add logic to order js

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,8 +1,30 @@
 import React from 'react';
-
+import { formatPrice } from '../helpers';
 class Order extends React.Component {
   render() {
-    return <div className="order">Inventoresdsy!!!</div>;
+    const orderIds = Object.keys(this.props.order);
+    const total = orderIds.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key];
+      const count = this.props.order[key];
+      const isAvailabe = fish && fish.status === 'available';
+
+      if (isAvailabe) {
+        return prevTotal + count * fish.price;
+      }
+      return prevTotal;
+    }, 0);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        {orderIds}
+        <div className="total">
+          <strong>{formatPrice(total)}</strong>
+        </div>
+        <ul>
+          <li></li>
+        </ul>
+      </div>
+    );
   }
 }
 
